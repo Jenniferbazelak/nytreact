89656205369526f76c0f50f366e20be5e4c6f307
Baz <jennigoldstein@gmail.com>
2018-09-23 09:05:34 -0500

    app.js renders page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Container from "./components/Container";
+import Results from "./components/Results";
+import Saved from "./components/Saved";
+import Search from "./components/Search";
+
 
 class App extends Component {
+  state = {
+    
+  };
+  
+  
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+           <Container>
+        <Header/>
+        <Search/>
+        <Results/>
+        <Saved/>
+        </Container>
       </div>
     );
   }
